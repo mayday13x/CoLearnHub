3055e2354e99a1b3bb01603806ffe72b891a030a
NxT1me <52325097+goncalocsousa1@users.noreply.github.com>
2025-05-19 16:58:45 +0100

    add: splashScreen

---
+++ app/src/main/java/com/example/colearnhub/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.colearnhub
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
